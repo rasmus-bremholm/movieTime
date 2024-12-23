8568306c2a611ea3935a6f3fe0e7e57197f42b65
Rasmus Bremholm <rasmus.brem@gmail.com>
2024-12-23 09:49:07 +0100

    Added two components

---
+++ src/router/index.ts
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomePage from "../components/HomePage.vue";
+import Movie from "../components/Movie.vue";
+
+export default createRouter({
+	history: createWebHistory("/"),
+	routes: [
+		{
+			path: "/",
+			name: "Home",
+			component: HomePage,
+		},
+		{
+			path: "/:movie",
+			name: "Movie",
+			component: Movie,
+			props: true,
+		},
+	],
+});
