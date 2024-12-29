cd9f54e4fac6756b4b4467d657d45bb2aecbbb1c
Rasmus Bremholm <rasmus.brem@gmail.com>
2024-12-29 12:26:38 +0100

    Card CSS hover

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -17,15 +17,47 @@ export default defineComponent({
 		<img
 			:src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`"
 			:alt="movie.title ? `Poster for ${movie.title}` : 'Movie Poster'" />
+		<div class="movieCardDetails">
+			<p>{{ movie.vote_average }}</p>
+			<p>{{ movie.genre_ids }}</p>
+			<h3>{{ movie.title }}</h3>
+			<p>{{ movie.overview }}</p>
+		</div>
 	</div>
 </template>
 <style scoped>
 .movieCard {
-	display: flex;
+	position: relative;
 	height: 370px;
 	width: 245px;
 	overflow: hidden;
 	background-color: #757575;
 	border-radius: 15px;
+}
+
+.movieCard img {
+	display: block;
+	width: 100%;
+	border-radius: 15px;
+	transition: transform 0.3s ease;
+}
+
+.movieCard:hover img {
+	transform: scale(1.1);
+}
+
+.movieCardDetails {
+	position: absolute;
+	bottom: 0;
+	left: 0;
+	right: 0;
+	background-color: rgba(0, 0, 0, 0.8);
+	padding: 1rem; /* 16px */
+	transform: translateY(100%);
+	transition: transform 0.2s ease;
+}
+
+.movieCard:hover .movieCardDetails {
+	transform: translateY(0);
 }
 </style>
