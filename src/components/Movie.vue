dd6311cefea0a586b4806c003e751002c05486c4
Rasmus Bremholm <rasmus.brem@gmail.com>
2025-01-07 09:08:24 +0100

    Movie component and Router

---
+++ src/components/Movie.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import MovieCard from "./MovieCard.vue";
+
+export default defineComponent({
+	name: "Movie",
+    components: {
+        
+    }
+});
+</script>
