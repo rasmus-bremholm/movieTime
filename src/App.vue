0007e762780211e53997f29e91dbc2198ca2e5ae
Rasmus Bremholm <rasmus.brem@gmail.com>
2025-02-13 07:36:50 +0100

    STORA FLYTTEN

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,16 @@
 export default {
 	name: "App",
 };
+
+/*
+Ok breakdown av vad vi ska göra idag. Vi ska egentligen flytta ut Navbar från routerview då det var idiotiskt.
+Så först och främst måste vi flytta logiken från HomePage.vue -> App.vue utan att ha sönder något.
+
+Var börjar vi? Vi testar bara att flytta ut Rubriken så ser vi vad som går sönder. Vi ska bara göra en commit först och därav denna kommentar.
+
+
+
+*/
 </script>
 
 <template>
